1f9a39696454d8c4663df5f887345784130cd42b
Al <dmitro.panov@gmail.com>
2016-03-30 06:45:33 +0300

    track issue with REDIS_URL

--- config/initializers/split.rb
+++ config/initializers/split.rb
@@ -1,7 +1,13 @@
 Split.configure do |config|
   config.experiments = YAML.load_file "config/experiments.yml"
 end
+puts "!"*100 + " Started"
+puts "ENV.fetch(\"REDIS_URL\", #{ENV.fetch("REDIS_URL")}): "
 
+require 'uri'
+puts "URI: #{URI(ENV.fetch("REDIS_URL"))}"
+
+puts "Finished!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1111"
 Split.redis = ENV.fetch("REDIS_URL")
 
 Split::Dashboard.use Rack::Auth::Basic do |username, password|
